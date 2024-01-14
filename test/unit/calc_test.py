8c9a474979e2fb514673b82d478e152c7f4d4721
hernancastel <152933814+hernancastel@users.noreply.github.com>
2024-01-14 18:21:06 -0300

    aumentando cobertura

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -25,7 +25,6 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(1, self.calc.divide(2, 2))
         self.assertEqual(1.5, self.calc.divide(3, 2))
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
-        self.assertRaises(TypeError, self.calc.divide, 2, 0)
 
     def test_add_method_fails_with_nan_parameter(self):
         self.assertRaises(TypeError, self.calc.add, "2", 2)
@@ -40,6 +39,7 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
         self.assertRaises(TypeError, self.calc.divide, 2, "2")
         self.assertRaises(TypeError, self.calc.divide, "2", "2")
+        self.assertRaises(TypeError, self.calc.divide, 2, 0)
 
     def test_multiply_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.multiply(2, 2))
